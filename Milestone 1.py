95d727a9768055b41b1a9526eb55d15d9a395ea6
José Reyes <josereyg21@gmail.com>
2021-08-02 18:44:05 -0600

    Update

--- Milestone 1.py
+++ Milestone 1.py
@@ -13,8 +13,6 @@ def movies_l():
         title, director, year = i['title'], i['director'], i['year']
         print(f'{title}     {director}     {year} ')
 
-
-
     
 def movies_f():
     x = input('What movie are you looking for? You can look by Name, Director or Year  ')
@@ -22,19 +20,22 @@ def movies_f():
         if x == movies[i]['title'] or x == movies[i]['director'] or x == movies[i]['year']:
             title, director, year = movies[i]['title'], movies[i]['director'], movies[i]['year']
             print(f'{title}     {director}     {year} ')
-        
-MENU_PROMPT = "\nEnter 'a' to add a movie, 'l' to see your movies, 'f' to find a movie by title, or 'q' to quit: "
-movies = []
 
-while True:
-    main_input = input(MENU_PROMPT)
-    if main_input == "a":
-        movies_a()
-    elif main_input == "l":
-        movies_l()
-    elif main_input == "f":
-        movies_f()
-    elif main_input == "q":
-        break
-    else:
-        print('Unknown command. Please try again.')
+            
+def menu_movies():
+    MENU_PROMPT = "\nEnter 'a' to add a movie, 'l' to see your movies, 'f' to find a movie by title, or 'q' to quit: "
+    
+    while True:
+        main_input = input(MENU_PROMPT)
+        if main_input == "a":
+            movies_a()
+        elif main_input == "l":
+            movies_l()
+        elif main_input == "f":
+            movies_f()
+        elif main_input == "q":
+            break
+        else:
+            print('Unknown command. Please try again.')
+movies = []
+menu_movies()
